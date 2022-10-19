9aab64dfb3fd76b6cc63fed041998164162daf2e
humbertovacu <humbertovacu@gmail.com>
2022-10-19 19:29:56 +0200

    iridium fix

--- js/index.js
+++ js/index.js
@@ -1,5 +1,3 @@
-window.onload = startGame();
-
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d'); 
 const rocketCanvas = document.getElementById("rocket-canvas");
